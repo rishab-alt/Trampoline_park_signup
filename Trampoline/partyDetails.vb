bdff7c11132545f59b23dc896c62f9cb5de841d1
Rish Saddiq <1517120@stu.tmc.ac.uk>
2024-02-09 11:55:13 +0000

    modify code

--- Trampoline/partyDetails.vb
+++ Trampoline/partyDetails.vb
@@ -1,4 +1,6 @@
-﻿Public Class partyDetails
+﻿Imports System.IO
+
+Public Class PartyDetails
     Dim adminCost = 10
     Dim heliumBalloon = 50
     Dim partyBag = 3
@@ -8,66 +10,65 @@
     Dim cakeSmall = 5
     Dim cakeMed = 20
     Dim cakeLarge = 35
-    Dim customCake As Integer
-    Dim partyDiscount As Integer
     Dim balloon_max = 250
 
-    Private Sub partyDetails_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        numberPeople.Items.Add("1")
-        numberPeople.Items.Add("2")
-        numberPeople.Items.Add("3")
-        numberPeople.Items.Add("4")
-        numberPeople.Items.Add("5")
-        numberPeople.Items.Add("6")
+    Private Sub PartyDetails_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        InitializeComboBoxes()
+    End Sub
 
-        HaveSocks.Items.Add("Yes")
-        HaveSocks.Items.Add("No")
-
-        cakerequired.Items.Add("Small")
-        cakerequired.Items.Add("Medium")
-        cakerequired.Items.Add("Large ")
+    Private Sub InitializeComboBoxes()
+        numberPeople.Items.AddRange({"1", "2", "3", "4", "5", "6"})
+        HaveSocks.Items.AddRange({"Yes", "No"})
+        cakerequired.Items.AddRange({"Small", "Medium", "Large"})
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         If Balloonchoice.Text > balloon_max Then
             MessageBox.Show("Maximum Balloons Reached, Sorry!")
         Else
-            If HaveSocks.Text = "Yes" Then
-                result.Text = "Number of people is " & numberPeople.Text & vbCrLf &
-                      "Has socks: Yes" & vbCrLf &
-                      "Admin fee is: " & adminCost & vbCrLf &
-                      "Number of balloons is: " & Balloonchoice.Text
+            Dim resultText As String = "Number of people is " & numberPeople.Text & vbCrLf &
+                                       "Has socks: " & HaveSocks.Text & vbCrLf &
+                                       "Admin fee is: " & adminCost & vbCrLf &
+                                       "Number of balloons is: " & Balloonchoice.Text
+
+            If cakerequired.SelectedItem IsNot Nothing Then
+                Select Case cakerequired.SelectedItem.ToString()
+                    Case "Small"
+                        resultText &= vbCrLf & "Cake choice is: Small" & vbCrLf & "Cake price is " & cakeSmall
+                    Case "Medium"
+                        resultText &= vbCrLf & "Cake choice is: Medium" & vbCrLf & "Cake price is " & cakeMed
+                    Case "Large"
+                        resultText &= vbCrLf & "Cake choice is: Large" & vbCrLf & "Cake price is " & cakeLarge
+                    Case Else
+                        MessageBox.Show("Error: Selected cake size not recognized.")
+                End Select
             Else
-                MessageBox.Show("Invalid")
+                MessageBox.Show("Error: No cake size selected.")
+            End If
+
+            result.Text = resultText
+
+            ' Prompt user to select a file path
+            Dim saveFileDialog As New SaveFileDialog()
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt"
+            saveFileDialog.Title = "Save Party Details"
+            saveFileDialog.ShowDialog()
+
+            If Not String.IsNullOrEmpty(saveFileDialog.FileName) Then
+                ' Save party details to file
+                SavePartyDetails(saveFileDialog.FileName, resultText)
             End If
         End If
-        If cakerequired.SelectedItem IsNot Nothing Then
-            If cakerequired.SelectedItem = "Small" Then
-                MessageBox.Show("Cake price is " & cakeSmall)
-                result.Text = "Number of people: " & numberPeople.Text & vbCrLf &
-                              "Has socks: Yes" & vbCrLf &
-                              "Admin fee is: " & adminCost & vbCrLf &
-                              "Number of balloons: " & Balloonchoice.Text & vbCrLf &
-                              "Cake choice is: Small"
-            ElseIf cakerequired.SelectedItem = "Medium" Then
-                MessageBox.Show("Cake price is " & cakeMed)
-                result.Text = "Number of people: " & numberPeople.Text & vbCrLf &
-                              "Has socks: Yes" & vbCrLf &
-                              "Admin fee is: " & adminCost & vbCrLf &
-                              "Number of balloons: " & Balloonchoice.Text & vbCrLf &
-                              "Cake choice is: Medium"
-            ElseIf cakerequired.SelectedItem = "Large" Then
-                MessageBox.Show("Cake price is " & cakeLarge)
-                result.Text = "Number of people: " & numberPeople.Text & vbCrLf &
-                              "Has socks: Yes" & vbCrLf &
-                              "Admin fee is: " & adminCost & vbCrLf &
-                              "Number of balloons: " & Balloonchoice.Text & vbCrLf &
-                              "Cake choice is: Large"
-            Else
-                MessageBox.Show("Error: Selected cake size not recognized.")
-            End If
-        Else
-            MessageBox.Show("Error: No cake size selected.")
-        End If
+    End Sub
+
+    Private Sub SavePartyDetails(filePath As String, partyDetails As String)
+        Try
+            Using writer As New StreamWriter(filePath)
+                writer.WriteLine(partyDetails)
+            End Using
+            MessageBox.Show("Party details saved to file successfully." & vbCrLf & "File Path: " & filePath)
+        Catch ex As Exception
+            MessageBox.Show("Error saving party details to file: " & ex.Message)
+        End Try
     End Sub
 End Class
